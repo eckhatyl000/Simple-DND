d6462d080a28954f373a4a0af7bc194315cf219a
Tyler E <ty-ty555@live.com>
2023-06-13 15:48:51 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,11 @@ const loginRoute = require('./routes/loginRoutes');
 const creatAccountRoute = require('./routes/createAccountRoutes');
 const authRoute = require('./routes/authRoutes');
 const characterRoute = require('./routes/characterRoutes');
+const { registerUser, loginUser } = require('./controllers/authController');
 const { createCharacter, getCharacterById } = require('./controllers/characterController');
 
-
-
-
 let users = [];
-
 let characters = [];
-
 
 app.use(bodyParser.json());
 
@@ -22,15 +18,10 @@ app.use('/', creatAccountRoute);
 app.use('/', authRoute);
 app.use('/', characterRoute);
 
-
 app.post('/register', registerUser);
 app.post('/login', loginUser);
-
 app.post('/characters', createCharacter);
-
 app.get('/characters/:id', getCharacterById);
-
-
 
 app.listen(3000, () => console.log('Server started on port 3000.'));
 
