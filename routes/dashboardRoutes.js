776c3d294610c3f56e2cadcd086f249d42005b42
Tyler E <ty-ty555@live.com>
2023-06-13 16:53:48 -0600

    Update dashboardRoutes.js

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,27 +1,14 @@
 const path = require('path');
-const User = require('../models/user');
-const Character = require('../models/characters');
+const characters = require('../data/characters'); // Assuming you have a file containing character data
 
-exports.dashboard = async (req, res) => {
+exports.dashboard = (req, res) => {
     try {
-        // Retrieve user data from the session
-        const userId = req.session.userId;
-
-        // Retrieve user details from the database
-        const user = await User.findById(userId);
-
-        // If the user does not exist, return an error response
-        if (!user) {
-            return res.status(404).json({ error: 'User not found' });
-        }
-
-        // Retrieve character details for the user from the database
-        const characters = await Character.find();
-
-        // Serve the static file
-        res.sendFile(path.join(__dirname, '..', 'Public', 'Dashboard', 'dashboard.html'));
+        // Serve the static file along with the character data
+        res.status(200).sendFile(path.join(__dirname, '..', 'Public', 'Dashboard', 'dashboard.html'), {
+            characters: characters
+        });
     } catch (error) {
-        // Handle any errors that occur during data retrieval or file serving
+        // Handle any errors that occur during file serving
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
