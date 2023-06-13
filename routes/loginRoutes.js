1679158330693f7fbe8da62675e2d53e03178bf6
Tyler E <ty-ty555@live.com>
2023-06-13 14:41:13 -0600

    Update loginRoutes.js

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile('login.html', { root: 'public' });
+    res.sendFile(path.join(__dirname, '../public/login.html'));
 });
 
 router.post('/', (req, res) => {
