06cf8b6c95b0be26e931626bcfdf9c3dc2a23ddc
Tyler E <ty-ty555@live.com>
2023-06-14 22:40:14 -0600

    Update scripts.js

--- Public/account-page/scripts.js
+++ Public/account-page/scripts.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', function () {
-    // Make a GET request to fetch the user account information from the server
+    
     fetch('/api/account')
         .then((response) => {
             if (!response.ok) {
@@ -8,7 +8,7 @@ window.addEventListener('DOMContentLoaded', function () {
             return response.json();
         })
         .then((data) => {
-            // Update account information in the HTML
+            
             document.getElementById('username').textContent = data.username;
             document.getElementById('email').textContent = data.email;
         })
