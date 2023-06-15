5c2c4823d038b985d43b4341901277aa442f264d
Tyler E <ty-ty555@live.com>
2023-06-14 20:57:36 -0600

    path fixing

--- Public/Login/create-account/create-account.js
+++ Public/Login/create-account/create-account.js
@@ -31,7 +31,7 @@ document.getElementById('createAccountForm').addEventListener('submit', function
             console.log(data);
             
             if (data.id) {
-                window.location.href = 'Public/Login/login.html';
+                window.location.href = '../../login.html';
             } else {
                 alert('Error: ' + data.error);
             }
@@ -43,6 +43,6 @@ document.getElementById('createAccountForm').addEventListener('submit', function
 
 document.getElementById('loginForm').addEventListener('click', function (event) {
     event.preventDefault();
-    window.location.href = 'Public/Login/login.html';
+    window.location.href = '../../login.html';
 });
 
